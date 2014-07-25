2d84010872f15e1243e9f494acd92b0a0a51b4e7
Jake Wharton <jw@squareup.com>
2014-07-25 00:34:58 -0700

    Directories and imports test.

--- src/test/java/com/jakewharton/twirl/IntegrationTest.java
+++ src/test/java/com/jakewharton/twirl/IntegrationTest.java
@@ -1,11 +1,13 @@
 package com.jakewharton.twirl;
 
+import com.google.common.io.Files;
 import java.io.File;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.resources.TestResources;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class IntegrationTest {
@@ -19,6 +21,46 @@ public final class IntegrationTest {
 
     File fooBarBazTemplate = new File(basedir, "target/generated-sources/twirl/foo/bar/html/baz.template.scala");
     assertThat(fooBarBazTemplate).exists();
+  }
+
+  @Test public void templateDirectory() throws Exception {
+    File basedir = resources.getBasedir("template-directory");
+
+    rule.executeMojo(basedir, "compile");
+
+    File fooBarBazTemplate = new File(basedir, "target/generated-sources/twirl/foo/bar/html/baz.template.scala");
+    assertThat(fooBarBazTemplate).exists();
+  }
+
+  @Test public void outputDirectory() throws Exception {
+    File basedir = resources.getBasedir("output-directory");
+
+    rule.executeMojo(basedir, "compile");
+
+    File fooBarBazTemplate = new File(basedir, "target/twirl-templates/foo/bar/html/baz.template.scala");
+    assertThat(fooBarBazTemplate).exists();
+  }
+
+  @Test public void imports() throws Exception {
+    File basedir = resources.getBasedir("imports");
+
+    rule.executeMojo(basedir, "compile");
+
+    File fooBarBazTemplate = new File(basedir, "target/generated-sources/twirl/foo/bar/html/baz.template.scala");
+    assertThat(fooBarBazTemplate).exists();
+    String fooBarBaz = Files.toString(fooBarBazTemplate, UTF_8);
+    assertThat(fooBarBaz).contains("import my.other.Thing");
+  }
+
+  @Test public void importReplacement() throws Exception {
+    File basedir = resources.getBasedir("import-replacement");
+
+    rule.executeMojo(basedir, "compile");
+
+    File fooBarBazTemplate = new File(basedir, "target/generated-sources/twirl/foo/bar/html/baz.template.scala");
+    assertThat(fooBarBazTemplate).exists();
+    String fooBarBaz = Files.toString(fooBarBazTemplate, UTF_8);
+    assertThat(fooBarBaz).contains("import my.other.html.Thing");
   }
 
   @Test public void excludes() throws Exception {
