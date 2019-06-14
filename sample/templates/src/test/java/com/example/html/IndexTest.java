073b75ed1dc013cc532e35fa4abd3705087fd1e9
Dimo Velev <dimo.velev@gmail.com>
2019-06-14 12:44:28 +0200

    Upgraded to scala 2.12.8 and twirl 1.4.1 so that it is usable with JDK > 1.8 (e.g. 11)

--- sample/templates/src/test/java/com/example/html/IndexTest.java
+++ sample/templates/src/test/java/com/example/html/IndexTest.java
@@ -7,19 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class IndexTest {
   @Test public void indexWithTemplate() {
     String actual = Index.render().toString();
-    String expected = String.format("%n"
-        + "<!DOCTYPE html>%n"
-        + "<html>%n"
-        + "  <head>%n"
-        + "    <title>Index - Example</title>%n"
-        + "  </head>%n"
-        + "  <body>%n"
-        + "%n"
-        + "<h1>Hello, world!</h1>%n"
-        + "%n"
-        + "  </body>%n"
-        + "</html>%n"
-        + "%n");
+    String expected = String.format("<!DOCTYPE html>\n"
+        + "<html>\n"
+        + "  <head>\n"
+        + "    <title>Index - Example</title>\n"
+        + "  </head>\n"
+        + "  <body>\n"
+        + "\n"
+        + "<h1>Hello, world!</h1>\n"
+        + "\n"
+        + "  </body>\n"
+        + "</html>\n"
+        + "\n");
     assertThat(actual).isEqualTo(expected);
   }
 }
